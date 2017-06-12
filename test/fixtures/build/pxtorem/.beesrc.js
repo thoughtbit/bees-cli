79062f2bd1382006d71eda4178ffaa65469bac7d
moocss <moocss@gmail.com>
2017-06-12 10:13:45 +0800

    pxtorem test

--- test/fixtures/build/pxtorem/.beesrc.js
+++ test/fixtures/build/pxtorem/.beesrc.js
@@ -3,6 +3,7 @@ import pxtorem from 'postcss-pxtorem';
 export default {
   devtool: "",
   extraPostCSSPlugins: [
+    require('postcss-flexbugs-fixes'),
     pxtorem({
       rootValue: 100,
       propWhiteList: [],
